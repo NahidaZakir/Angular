1b30119f1f372ad576627b16494a3f34df4a6b42
Nahida Zakir <nahidazakir15@gmail.com>
2024-02-21 16:02:27 +0600

    1st project

--- basics-one/src/app/recipes/recipe-list/recipe-list.component.ts
+++ basics-one/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { Recipe } from '../recipe.model';}
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'Barbeque','https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+    new Recipe('A Test Recipe', 'Barbeque','https://images.unsplash.com/photo-1498531872221-ce6d6216be71?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHx8')
+  ];
 }
--- basics-one/src/app/shopping-list/shopping-list.component.ts
+++ basics-one/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredients[]=[
+    new Ingredients('apples',5),
+    new Ingredients('tomatoes',10)
+  ]
 }
